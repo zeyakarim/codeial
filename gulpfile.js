c92948511308b5190b4f704e193b581f7cac4fa7
zeya karim <zeyakarim470@gmail.com>
2022-07-08 12:15:06 +0530

    Add view-helper file to run assets file

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,27 @@
 const gulp = require('gulp');
 
-
+// gulp-sass convert sass to css
 const sass = require('gulp-sass')(require('node-sass'));
+
+// gulp-cssnano compressed the file,like remove the space,do variable name shorter 
+//and all code contain one line
 const cssnano = require('gulp-cssnano');
+
+// gulp-rev add the hash to file name
 const rev = require('gulp-rev');
+
+// gulp-uglify-es compress the js file,like remove the space,do variable name shorter
+// and all code contain one line
 const uglify = require('gulp-uglify-es').default;
+
+// gulp-imgagemin compress the image file,like remove the space,do variable name shorter
+// and all code contain one line
 const imagemin = require('gulp-imagemin');
+
+// using del i will previous file
 const del = require('del');
 
+// gulp compressed task is css
 gulp.task('css',function(done){
     console.log('minifying css...')
     gulp.src('./assets/scss/**/*.scss')
@@ -26,7 +40,7 @@ gulp.task('css',function(done){
     done();
 });
 
-// next compressed task is js
+// gulp compressed task is js
 gulp.task('js',function(done){
     console.log('minifying js...');
     gulp.src('./assets/**/*.js')
@@ -42,7 +56,7 @@ gulp.task('js',function(done){
 });
 
 
-// next compressed task is images
+// gulp compressed task is images
 gulp.task('images',function(done){
     console.log('minifying images..');
     gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
@@ -59,7 +73,8 @@ gulp.task('images',function(done){
 
 // empty the public/assets diretory
 gulp.task('clean:assets',function(done){
-    del.sync('./public/assets');
+    // del.sync('./public/assets');
+    del.sync(['./public/assets'], { force:true });
     done();
 });
 
